5f595a266e72bbe2aed21d1887847ff28c50eafd
x-j <xjasienski@gmail.com>
2016-04-13 03:03:59 +0200

    Part I functional, code is neat.

---
+++ Paint It, Black/Paint It, Black/MainForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Paint_It__Black {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.segmentLabel = new System.Windows.Forms.Label();
+            this.colorPickerButton = new System.Windows.Forms.Button();
+            this.hexButton = new System.Windows.Forms.Button();
+            this.quadrangleButton = new System.Windows.Forms.Button();
+            this.triangleButton = new System.Windows.Forms.Button();
+            this.segmentButton = new System.Windows.Forms.Button();
+            this.drawingPanel = new System.Windows.Forms.Panel();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.colorButtonExplainingLabel = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(783, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer.Location = new System.Drawing.Point(12, 27);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.colorButtonExplainingLabel);
+            this.splitContainer.Panel1.Controls.Add(this.label3);
+            this.splitContainer.Panel1.Controls.Add(this.label2);
+            this.splitContainer.Panel1.Controls.Add(this.label1);
+            this.splitContainer.Panel1.Controls.Add(this.segmentLabel);
+            this.splitContainer.Panel1.Controls.Add(this.colorPickerButton);
+            this.splitContainer.Panel1.Controls.Add(this.hexButton);
+            this.splitContainer.Panel1.Controls.Add(this.quadrangleButton);
+            this.splitContainer.Panel1.Controls.Add(this.triangleButton);
+            this.splitContainer.Panel1.Controls.Add(this.segmentButton);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.drawingPanel);
+            this.splitContainer.Size = new System.Drawing.Size(759, 395);
+            this.splitContainer.SplitterDistance = 349;
+            this.splitContainer.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(126, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Tag = "4";
+            this.label3.Text = "Click 2 more points to draw a segment.";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(126, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Tag = "3";
+            this.label2.Text = "Click 2 more points to draw a segment.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(126, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Tag = "2";
+            this.label1.Text = "Click 2 more points to draw a segment.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Visible = false;
+            // 
+            // segmentLabel
+            // 
+            this.segmentLabel.Location = new System.Drawing.Point(126, 20);
+            this.segmentLabel.Name = "segmentLabel";
+            this.segmentLabel.Size = new System.Drawing.Size(182, 36);
+            this.segmentLabel.TabIndex = 2;
+            this.segmentLabel.Tag = "1";
+            this.segmentLabel.Text = "Click 2 more points to draw a segment.";
+            this.segmentLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.segmentLabel.Visible = false;
+            // 
+            // colorPickerButton
+            // 
+            this.colorPickerButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.colorPickerButton.BackColor = System.Drawing.SystemColors.ControlText;
+            this.colorPickerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorPickerButton.ForeColor = System.Drawing.Color.White;
+            this.colorPickerButton.Location = new System.Drawing.Point(3, 339);
+            this.colorPickerButton.Name = "colorPickerButton";
+            this.colorPickerButton.Size = new System.Drawing.Size(134, 38);
+            this.colorPickerButton.TabIndex = 1;
+            this.colorPickerButton.Tag = "5";
+            this.colorPickerButton.UseVisualStyleBackColor = false;
+            this.colorPickerButton.Click += new System.EventHandler(this.colorPickerButton_Click);
+            // 
+            // hexButton
+            // 
+            this.hexButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.hexButton.FlatAppearance.BorderSize = 2;
+            this.hexButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hexButton.Location = new System.Drawing.Point(3, 146);
+            this.hexButton.Name = "hexButton";
+            this.hexButton.Size = new System.Drawing.Size(117, 36);
+            this.hexButton.TabIndex = 0;
+            this.hexButton.Tag = "4";
+            this.hexButton.Text = "Hexagon";
+            this.hexButton.UseVisualStyleBackColor = false;
+            // 
+            // quadrangleButton
+            // 
+            this.quadrangleButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.quadrangleButton.FlatAppearance.BorderSize = 2;
+            this.quadrangleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quadrangleButton.Location = new System.Drawing.Point(3, 104);
+            this.quadrangleButton.Name = "quadrangleButton";
+            this.quadrangleButton.Size = new System.Drawing.Size(117, 36);
+            this.quadrangleButton.TabIndex = 0;
+            this.quadrangleButton.Tag = "3";
+            this.quadrangleButton.Text = "Quadrangle";
+            this.quadrangleButton.UseVisualStyleBackColor = false;
+            // 
+            // triangleButton
+            // 
+            this.triangleButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.triangleButton.FlatAppearance.BorderSize = 2;
+            this.triangleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.triangleButton.Location = new System.Drawing.Point(3, 62);
+            this.triangleButton.Name = "triangleButton";
+            this.triangleButton.Size = new System.Drawing.Size(117, 36);
+            this.triangleButton.TabIndex = 0;
+            this.triangleButton.Tag = "2";
+            this.triangleButton.Text = "Triangle";
+            this.triangleButton.UseVisualStyleBackColor = false;
+            // 
+            // segmentButton
+            // 
+            this.segmentButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.segmentButton.FlatAppearance.BorderSize = 2;
+            this.segmentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.segmentButton.Location = new System.Drawing.Point(3, 20);
+            this.segmentButton.Name = "segmentButton";
+            this.segmentButton.Size = new System.Drawing.Size(117, 36);
+            this.segmentButton.TabIndex = 0;
+            this.segmentButton.Tag = "1";
+            this.segmentButton.Text = "Segment";
+            this.segmentButton.UseVisualStyleBackColor = false;
+            // 
+            // drawingPanel
+            // 
+            this.drawingPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.drawingPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.drawingPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawingPanel.Location = new System.Drawing.Point(0, 0);
+            this.drawingPanel.Name = "drawingPanel";
+            this.drawingPanel.Size = new System.Drawing.Size(406, 395);
+            this.drawingPanel.TabIndex = 0;
+            this.drawingPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingPanel_Paint);
+            // 
+            // colorButtonExplainingLabel
+            // 
+            this.colorButtonExplainingLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.colorButtonExplainingLabel.AutoSize = true;
+            this.colorButtonExplainingLabel.Location = new System.Drawing.Point(3, 323);
+            this.colorButtonExplainingLabel.Name = "colorButtonExplainingLabel";
+            this.colorButtonExplainingLabel.Size = new System.Drawing.Size(119, 13);
+            this.colorButtonExplainingLabel.TabIndex = 3;
+            this.colorButtonExplainingLabel.Tag = "nothing to see here";
+            this.colorButtonExplainingLabel.Text = "Color of the new shape:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(783, 434);
+            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.MinimumSize = new System.Drawing.Size(700, 380);
+            this.Name = "MainForm";
+            this.Text = "This is not Paint";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Panel drawingPanel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label segmentLabel;
+        private System.Windows.Forms.Button colorPickerButton;
+        private System.Windows.Forms.Button hexButton;
+        private System.Windows.Forms.Button quadrangleButton;
+        private System.Windows.Forms.Button triangleButton;
+        private System.Windows.Forms.Button segmentButton;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Label colorButtonExplainingLabel;
+    }
+}
+
---
+++ Paint It, Black/Paint It, Black/MainForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Paint_It__Black {
+    public partial class MainForm : Form {
+
+        //lists containing the controls:
+        List<Button> buttons = new List<Button>();
+        List<Label> labels = new List<Label>();
+
+        byte[] clickRequirements = { 0, 2, 3, 4, 6 };  //useful 
+        string[] shapeNames = { "nulloid", "segment", "triangle", "quadrangle", "hexagon" };    //less useful
+
+        byte selectedShape = 0; //if this is 0 it means we're not drawing at this moment
+
+        List<Point> clickedPoints = new List<Point>();
+
+        List<Shape> shapes = new List<Shape>();
+
+        private class Shape {
+
+            public List<Point> Vertices { get; private set; }
+            public Color Color { get; private set; }
+
+            public Shape(List<Point> pointsClicked, Color color) {
+                Vertices = pointsClicked;
+                Color = color;
+            }
+        }
+
+        public MainForm() {
+            InitializeComponent();
+            //add controls to the lists, and by the way add event handlers to the buttons:
+            foreach (Control c in splitContainer.Panel1.Controls) {
+                if (c is Label && !c.Equals(colorButtonExplainingLabel)) labels.Add(c as Label);
+                if (c is Button && !c.Equals(colorPickerButton)) {
+                    buttons.Add(c as Button);
+                    c.Click += button_Click;
+                }
+            }
+            drawingPanel.Click += drawingPanel_Click;
+        }
+
+        private void button_Click(object sender, EventArgs e) {
+            selectedShape = byte.Parse((sender as Button).Tag as string);
+            foreach (var b in buttons) b.Enabled = false;
+            Label label = labels.Find(l => (l.Tag as string).Equals(selectedShape.ToString()));
+            label.Visible = true;
+            label.Text = "Click " + clickRequirements[selectedShape] + " more points to draw a " + shapeNames[selectedShape];
+        }
+
+        private void colorPickerButton_Click(object sender, EventArgs e) {
+            colorDialog.ShowDialog();
+            colorPickerButton.BackColor = colorDialog.Color;
+        }
+
+        private void drawingPanel_Click(object sender, EventArgs e) {
+            if (selectedShape > 0) {
+                MouseEventArgs args = e as MouseEventArgs;
+                clickedPoints.Add(new Point(args.X, args.Y));
+                UpdateLabel();
+                if (clickedPoints.Count == clickRequirements[selectedShape]) DrawShape();
+            } else {
+                //TODO: implement moving of the shapes here
+            }
+        }
+
+        private void UpdateLabel() {
+            Label label = labels.Find(l => (l.Tag as string).Equals(selectedShape.ToString()));
+            label.Text = "Click " + (clickRequirements[selectedShape] - clickedPoints.Count) + " more points to draw a " + shapeNames[selectedShape]+".";
+        }
+
+        private void DrawShape() {
+            Shape s = new Shape(new List<Point>(clickedPoints), colorDialog.Color);
+            shapes.Add(s);
+            drawingPanel.Refresh();
+            foreach (var b in buttons) b.Enabled = true;
+            foreach (var l in labels) l.Visible = false;
+            clickedPoints.Clear();
+            selectedShape = 0;
+        }
+
+        private void drawingPanel_Paint(object sender, PaintEventArgs e) {
+            foreach (var shape in shapes) {
+                Graphics g = drawingPanel.CreateGraphics();
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighSpeed;
+                if (shape.Vertices.Count == 2) g.DrawLine(new Pen(shape.Color, 2), shape.Vertices[0], shape.Vertices[1]);
+                else {
+                    Pen pen = new Pen(shape.Color, 2);
+                    g.DrawPolygon(pen, shape.Vertices.ToArray());
+                    g.FillPolygon(pen.Brush, shape.Vertices.ToArray());
+                    pen.Dispose();
+                }
+                g.Dispose();
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e) {
+            //we refresh everything!
+            foreach (var b in buttons) b.Enabled = true;
+            foreach (var l in labels) l.Visible = false;
+            shapes.Clear();
+            clickedPoints.Clear();
+            selectedShape = 0;
+            colorDialog.Color = Color.Black;
+            colorPickerButton.BackColor = colorDialog.Color;
+            drawingPanel.Invalidate();
+        }
+    }
+}
